0426b6e6221c21fe9a900394213750d95dd6c2c2
evijow <95678787+evijow@users.noreply.github.com>
2025-06-02 14:47:32 +0700

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -8,6 +8,7 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+
 echo $($PSQL "truncate games, teams restart identity cascade")
 cat games.csv | while IFS="," read year round winner opponent winner_goals opponent_goals
 
@@ -23,7 +24,7 @@ do
 			insert_winner=$($PSQL "insert into teams(name) values('$winner')")
 			if [[ $insert_winner == "INSERT 0 1" ]]
 			then
-				echo "inserted $winner"
+				echo "$insert_winner for $winner"
 			fi
 			#get new id
 			winner_id=$($PSQL "select team_id from teams where name='$winner'")
@@ -36,17 +37,12 @@ do
 			insert_opponent=$($PSQL "insert into teams(name) values('$opponent')")
 			if [[ $insert_opponent == "INSERT 0 1" ]]
 			then
-				echo "inserted $opponent"
+				echo "$insert_opponent for $opponent"
 			fi
 			#get new id
 			opponent_id=$($PSQL "select team_id from teams where name='$opponent'")
 		fi
 	#insert into game
 	insert_into=$($PSQL "insert into games(year,round,winner_id,opponent_id,winner_goals,opponent_goals) values($year,'$round',$winner_id,$opponent_id,$winner_goals,$opponent_goals)")
-	#if [[ $insert_into == "INSERT 0 1" ]]
-	#then
-	#	echo "inserted game: $winner vs $opponent"
-	#fi
 fi
 done
-
